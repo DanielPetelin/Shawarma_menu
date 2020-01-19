73063c5d905348e4ead956e67981882cbace5190
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-19 11:13:07 +0300

    Cost napitki

--- js/script.js
+++ js/script.js
@@ -14,3 +14,20 @@ function getResult() {
     document.getElementById('cost').innerHTML = cost;
     document.getElementById('kkal').innerHTML = kkal;
 };
+
+document.getElementById('resultn').onclick = getCostN;
+
+function getCostN() {
+    var menun = document.getElementsByClassName('menun');
+    var costn = 0;
+    var kkall = 0;
+
+    for (var i = 0; i < menun.length; i++) {
+        if (menun[i].checked) {
+            costn += parseFloat(menun[i].getAttribute('data-costn'));
+            kkall += parseFloat(menun[i].getAttribute('data-kkall'));
+        };
+    }
+    document.getElementById('costn').innerHTML = costn;
+    document.getElementById('kkall').innerHTML = kkall;
+};
